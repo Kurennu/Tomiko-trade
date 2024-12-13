84f31b3d5045d5f5b7719316ee84a63cdeecd1bb
buzzaggwd <125117936+buzzaggwd@users.noreply.github.com>
2024-12-14 09:43:51 +1000

    Саша проверь коммит!!

--- TomikoProject/main/views.py
+++ TomikoProject/main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .cars_models import Cars
 
-# Create your views here.
+def catalog_view(request): # Функция для чтения бд и передачи машинок в каталог
+    cars = Cars.objects.all() # Получаем все машинки из таблицы cars
+    return render(request, 'catalog.html', {'cars': cars}) # Передаем данные в шаблон
