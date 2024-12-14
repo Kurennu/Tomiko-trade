6892fee65a6b95dd32e533be016640fba76f7245
Kurennu <kurenu66@gmail.com>
2024-12-14 14:22:59 +1000

    models changes, add superuser

--- TomikoProject/main/views.py
+++ TomikoProject/main/views.py
@@ -1,9 +1,4 @@
 from django.shortcuts import render
-from .cars_models import Cars
 
 def index(request):
     return render(request, 'index.html')
-
-def catalog_view(request): # Функция для чтения бд и передачи машинок в каталог
-    cars = Cars.objects.all() # Получаем все машинки из таблицы cars
-    return render(request, 'catalog.html', {'cars': cars}) # Передаем данные в шаблон
