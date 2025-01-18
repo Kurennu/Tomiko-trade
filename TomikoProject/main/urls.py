733fc393410e63dcbf827f2ec3d94317e1fda4dc
Kurennu <kurenu66@gmail.com>
2025-01-18 13:53:19 +1000

    catalog done

--- TomikoProject/main/urls.py
+++ TomikoProject/main/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name='index'),
     path("contacts", views.contacts, name='contacts'),
     path("catalog/", views.catalog, name='catalog'),
+    path('catalog/<str:country>/', views.catalog, name='catalog_by_country'),
+    path('catalog/car/<int:car_id>/', views.car_detail, name='car_detail')
 ]
--- TomikoProject/main/views.py
+++ TomikoProject/main/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Cars, Brands
 import json
 
@@ -10,18 +10,6 @@ def index(request):
 def contacts(request):
     return render(request, 'contacts.html')
 
-# Функция для карточек и пагинатора
-# def car_catalog(request):
-#     cars = Cars.objects.all()
-#     items_per_page = 12 # Сколько карточек будет на странице (в дизайне вроде 12, так что пока 12)
-#     paginator = Paginator(cars, items_per_page)
-
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-
-#     return render(request, 'catalog.html', {'page_obj': page_obj})
-
- 
 def slider_view(request):
     cars_korea = Cars.objects.filter(brand_country__country='Корея')
     cars_japan = Cars.objects.filter(brand_country__country='Япония')
@@ -39,19 +27,56 @@ def slider_view(request):
     return render(request, 'index.html', context)
 
 
-def catalog(request):
+def catalog(request, country=None):
     queryset = Cars.objects.all()
- 
+    
+    country_mapping = {
+        'korea': 'Корея',
+        'japan': 'Япония',
+        'china': 'Китай'
+    }
+    
+    title_mapping = {
+        'korea': 'Автомобили из Кореи',
+        'japan': 'Автомобили из Японии',
+        'china': 'Автомобили из Китая'
+    }
+    
+
+    if country and country in country_mapping:
+        current_country = country_mapping[country]
+        queryset = queryset.filter(brand_country__country=current_country)
+        page_title = title_mapping[country]
+        
+
+        existing_brands = Cars.objects.filter(
+            brand_country__country=current_country
+        ).values_list('brand_country__brand', flat=True).distinct()
+    else:
+        page_title = 'Каталог автомобилей'
+        current_country = None
+        existing_brands = Cars.objects.values_list('brand_country__brand', flat=True).distinct()
+    
+    brands = list(existing_brands)
+    
+
+    models_by_brand = {}
+    for brand in brands:
+        models_query = Cars.objects.filter(brand_country__brand=brand)
+        if current_country:
+            models_query = models_query.filter(brand_country__country=current_country)
+        
+        models = list(models_query.values_list('model', flat=True).distinct())
+        models_by_brand[brand] = models
+
     brand = request.GET.get('brand')
     if brand:
         queryset = queryset.filter(brand_country__brand__iexact=brand)
     
-
     model = request.GET.get('model')
     if model:
         queryset = queryset.filter(model__iexact=model)
     
-
     year_from = request.GET.get('year_from')
     year_to = request.GET.get('year_to')
     if year_from:
@@ -69,9 +94,9 @@ def catalog(request):
     engine_from = request.GET.get('engine_from')
     engine_to = request.GET.get('engine_to')
     if engine_from:
-        queryset = queryset.filter(engine_volume__gte=int(engine_from))
+        queryset = queryset.filter(engine_volume__gte=float(engine_from))
     if engine_to:
-        queryset = queryset.filter(engine_volume__lte=int(engine_to))
+        queryset = queryset.filter(engine_volume__lte=float(engine_to))
     
     transmission = request.GET.get('transmission')
     if transmission:
@@ -85,21 +110,20 @@ def catalog(request):
     if color:
         queryset = queryset.filter(color__iexact=color)
     
-    existing_brands = Cars.objects.values_list('brand_country__brand', flat=True).distinct()
-    brands = list(existing_brands)
-
-    models_by_brand = {}
-    for brand in brands:
-        models = list(Cars.objects.filter(brand_country__brand=brand)
-                     .values_list('model', flat=True)
-                     .distinct())
-        models_by_brand[brand] = models
-
-    transmissions = list(Cars.objects.values_list('transmission', flat=True).distinct())
-    drives = list(Cars.objects.values_list('drive', flat=True).distinct())
-    colors = list(Cars.objects.values_list('color', flat=True).distinct())
+ 
+    transmissions_query = Cars.objects
+    drives_query = Cars.objects
+    colors_query = Cars.objects
     
-
+    if current_country:
+        transmissions_query = transmissions_query.filter(brand_country__country=current_country)
+        drives_query = drives_query.filter(brand_country__country=current_country)
+        colors_query = colors_query.filter(brand_country__country=current_country)
+    
+    transmissions = list(transmissions_query.values_list('transmission', flat=True).distinct())
+    drives = list(drives_query.values_list('drive', flat=True).distinct())
+    colors = list(colors_query.values_list('color', flat=True).distinct())
+    
     paginator = Paginator(queryset, 12)
     page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
@@ -111,6 +135,18 @@ def catalog(request):
         'transmissions_json': json.dumps(transmissions),
         'drives_json': json.dumps(drives),
         'colors_json': json.dumps(colors),
+        'page_title': page_title,
+        'current_country': country,
     }
     
     return render(request, 'catalog.html', context)
+
+def car_detail(request, car_id):
+    car = get_object_or_404(Cars, id=car_id)
+    similar_cars = Cars.objects.filter(brand_country__brand=car.brand_country.brand).exclude(id=car.id)[:4]
+    
+    context = {
+        'car': car,
+        'similar_cars': similar_cars,
+    }
+    return render(request, 'product.html', context)
